da516b41d58bc8131b1c5b4f02d9e44aeae5c804
100rbhsarin <saurabh007630@gmail.com>
2022-12-08 01:56:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+ 
+ const hourEl = document.querySelector(".hour")
+ const minuteEl = document.querySelector(".minute")
+ const secondEl = document.querySelector(".second")
+ const timeEl = document.querySelector(".time")
+ const dateEl = document.querySelector(".date")
+ const toggle = document.querySelector(".toggle")
+
+
+ const days =["Sunday","Monday", "Tuesday","Wednesday","thursday","Friday","Saturday"];
+ const months = ["JA","FE",'MR','AP','MY','JN','JL','AU','SE','OC','NV','DE']
+
+ toggle.addEventListener("click",(e) =>{
+    const html = document.querySelector('html')
+    if(html.classList.contains("dark")){
+        html.classList.remove("dark")
+        e.target.innerhtml = "Dark mode"
+        console.log("dark")
+    }
+    else{
+        html.classList.add("dark")
+        e.target.innerhtml = "light mode"
+    }
+ })
+ function setTime(){
+    const time = new Date();
+    const month = time.getMonth();
+    const day = time.getDay();
+    const date = time.getDate();
+    const hours = time.getHours();
+    const hoursForClock = hours % 12
+    const minutes = time.getMinutes()
+    const seconds = time.getSeconds()
+    const ampm = hours >= 12 ? "PM" : "AM" 
+
+
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+
+     minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+
+     secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+     timeEl.innerHTML = `${hoursForClock}:${minutes<10 ? `0${minutes}` : minutes} ${ampm}` 
+   
+     dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+
+ }
+//stackflow
+ 
+ const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+ setTime()
+
+ setInterval(setTime, 1000)
